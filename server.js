6892227a7585019539ab390510713f9d4bba2bbb
muturgan <caxap_8787@mail.ru>
2018-09-13 22:22:21 +0300

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 8888;
+
+io.on('connection', (socket) => {
+    const interval = setInterval(() => {
+        socket.emit('pinguin', 'Dmitry Guselnikov is a man');
+    }, 4000);
+
+    socket.on('disconnect', () => {
+        clearInterval(interval);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`pinguin listening on ${ PORT }`);
+});
